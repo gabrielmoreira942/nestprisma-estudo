d4e4f6e2280031a386abc6b7c6fe80fc1b608aba
gabriel <maboroshiken@gmail.com>
2024-02-06 00:22:20 -0300

    criando controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskController } from './task/task.controller';
+import { GroupController } from './group/group.controller';
+import { GroupService } from './group/group.service';
+import { GroupModule } from './group/group.module';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GroupModule, TaskModule],
+  controllers: [AppController, TaskController, GroupController],
+  providers: [AppService, GroupService, TaskService],
 })
 export class AppModule {}
